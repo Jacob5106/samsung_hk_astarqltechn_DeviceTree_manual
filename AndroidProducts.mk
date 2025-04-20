84c7cc0d01fecea126f03015c477cff2b57092ac
Jacob_yang <158026709@qq.com>
2025-04-20 12:57:09 +0800

    update

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,3 +7,7 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_astarqltechn.mk
+
+COMMON_LUNCH_CHOICES := omni_astarqltechn-user
+COMMON_LUNCH_CHOICES := omni_astarqltechn-userdebug
+COMMON_LUNCH_CHOICES := omni_astarqltechn-eng
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_astarqltechn-user
-add_lunch_combo omni_astarqltechn-userdebug
-add_lunch_combo omni_astarqltechn-eng
+# add_lunch_combo omni_astarqltechn-user
+# add_lunch_combo omni_astarqltechn-userdebug
+# add_lunch_combo omni_astarqltechn-eng
